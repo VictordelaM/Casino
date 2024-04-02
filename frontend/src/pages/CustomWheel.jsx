830e2ebcbef86eb88e2cab4396a9543705f1a7d1
VictordelaM <delamotte.v@gmail.com>
2024-04-03 01:22:49 +0200

    rad ist rund

--- frontend/src/pages/CustomWheel.jsx
+++ frontend/src/pages/CustomWheel.jsx
@@ -4,19 +4,22 @@ import './stylings/customWheel.css'
 
 const CustomWheel = () => {
   //!farbe funktioniert nicht
-  const arr = [{value:"0", color:'white'},{value:"10", color:'red'},{value:"20", color:'blue'}]
+  const arr = [{value:0, color:'white'},{value:10, color:'red'},{value:20, color:'blue'},{value:20, color:'blue'},{value:20, color:'blue'},{value:20, color:'blue'},{value:20, color:'blue'},{value:20, color:'blue'}]
   return (
     <div>
       <Nav/>
-
-     <div id='wheelBox'>
-          <div id="wheel">
-            {arr.map((e, i)=>{
-              i++
-              const dropname = 'drop'+i
-              return <div key={i} className='drop' id={dropname}>{e.value}</div>
-            })}
-          </div>
+      
+     <div className='wheelBody'>
+       <div id='wheelBox'>
+            <div className="spinBtn">Spin</div>
+            <div id="wheel">
+              {arr.map((e, i)=>{
+                i++
+                const dropname = 'drop'+i
+                return <div key={i} className='drop' id={dropname} style={{ transform: `rotate(${45*i}deg)` }}>{e.value}</div>
+              })}
+            </div>
+       </div>
      </div>
     </div>
   )
